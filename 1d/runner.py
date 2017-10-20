e6230ac16bc3073f54cf6d0fd72d136944e0f6e8
Graham Smith <grahamas@gmail.com>
2017-10-20 15:29:19 -0500

    Added support for parallel processes (same node).

--- 1d/runner.py
+++ 1d/runner.py
@@ -10,13 +10,16 @@
 import logging as log
 log.basicConfig(level=log.INFO)
 
+from multiprocessing import Pool
+from itertools import islice
+
 # Enable local imports from parent directory
 import sys
 sys.path.append("..")
 
 # Local imports
 from analyse import Results
-from args import load_args_from_file, inner_dcts_pop
+from args import load_args_from_file, inner_dcts_pop, Parametrizations
 from lattice import Lattice
 from diffeq import factories_dn
 import integrate
@@ -43,17 +46,28 @@ def simulate(*, lattice, solver, equation_name, **params):
 
     return activity
 
-@load_args_from_file()
-def run_simulation(*, run_name, results_params, 
-        model_modifications, **model_params):
-    """
-        Run the simulation resulting from simulate_neuman, and
-        save results.
-    """
-    model_params = {**model_params, **model_modifications}
-
+def sim_and_analyse(model_params):
+    run_name = model_params.pop('run_name')
+    results_params = model_params.pop('results_params')
     activity = simulate(**model_params)
     results = Results(data=activity, **results_params,
             run_name=run_name, model_params=model_params)
     results.analyse()
     log.info("All done with {}.".format(run_name))
+
+@load_args_from_file()
+def run_simulation(*, model_modifications, parent_dir=None, **base_model_params):
+    """
+        Run the simulation resulting from simulate_neuman, and
+        save results.
+    """
+    log.info("Starting parallel simulations...")
+
+    model_params_itr = Parametrizations({**base_model_params,
+        **model_modifications})
+
+    ### TESTING ###
+    log.info("Initializing pool...")
+    p = Pool(12)
+    log.info("Pool initialized.")
+    p.map(sim_and_analyse, islice(model_params_itr, 0, 10))
--- args.py
+++ args.py
@@ -10,6 +10,8 @@
 import json
 import os
 from functools import reduce
+import itertools
+from copy import deepcopy
 
 #region json helpers
 def read_json_file(filename):
@@ -72,3 +74,74 @@ def inner_dcts_pop(dct, l_keys):
             dct[key] for key in l_keys.
     '''
     return reduce(add_dcts, multikey(dct, l_keys))
+
+class ParameterSet(set):
+    def __init__(self, *args, name=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = name
+
+class Parametrizations(object):
+    '''
+        Takes dictionary of params (param_d) that possibly has sets
+        in place of parameters, and returns back the "product" of
+        parametrizations wrt those sets.
+
+        NOT PARALLEL!
+    '''
+    def __init__(self, param_d):
+        self.param_d = param_d
+        self.base_run_name = param_d['run_name']
+        self.set_path_l = []
+        self.set_name_l = []
+        self.set_l = []
+        self.find_all_set_paths(self.param_d, [])
+        self.set_product = itertools.product(*self.set_l)
+        self.cleanse_param_d()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        param_changes = next(self.set_product)
+        return self.new_param_d(param_changes)
+
+    def find_all_set_paths(self, level, this_path):
+        if isinstance(level, ParameterSet):
+            self.set_path_l += [this_path]
+            self.set_name_l += [level.name]
+            self.set_l += [level]
+            return
+        if isinstance(level, dict):
+            for key, value in level.items():
+                self.find_all_set_paths(value, this_path + [key])
+        elif isinstance(level, list):
+            for idx, value in enumerate(level):
+                self.find_all_set_paths(value, this_path + [idx])
+        return
+
+    def new_param_d(self, param_changes):
+        param_d_copy = deepcopy(self.param_d)
+        self.set_sets(param_d_copy, param_changes)
+        param_changes_str = map(str, param_changes)
+        mod_str = '_'.join([''.join(pair)
+            for pair in zip(self.set_name_l, param_changes_str)
+            if pair[0]])
+        param_d_copy['run_name'] = '_'.join([self.base_run_name, mod_str])
+        return param_d_copy
+
+    def set_sets(self, param_d, param_changes):
+        for path, change in zip(self.set_path_l, param_changes):
+            self.set_set(param_d, path, change)
+    def set_set(self, param_d, path, change):
+        '''
+            Iterate through parameters until you reach the level
+            before the change. Then exit the loop and use the idx_or_key
+            to make the change. Everything is by reference, so the change
+            carries over to the instance's copy.
+        '''
+        level = param_d
+        for idx_or_key in path[:-1]:
+            level = level[idx_or_key]
+        level[path[-1]] = change
+    def cleanse_param_d(self):
+        self.set_sets(self.param_d, [None] * len(self.set_path_l))
